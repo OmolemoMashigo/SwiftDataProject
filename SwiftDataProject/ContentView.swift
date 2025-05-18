4c4f3bdf6e6ecf885d7cb3a562888afddb023208
Omolemo Mashigo <omomashigo@gmail.com>
2025-05-18 23:09:23 +0200

    editing SwiftData model objects

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User] //a query sorting by our users' names
+    @State private var path = [User]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path){
+            List(users){ user in
+                NavigationLink(value: user){
+                    Text(user.name)
+                }
+            }
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self){ user in
+                EditUserView(user: user)
+            }
+            
+            .toolbar{
+                Button("add user", systemImage: "plus"){
+                    let user = User(name: "", city: "", date: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                }
+            }
         }
-        .padding()
     }
 }
 
