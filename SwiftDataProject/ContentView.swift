a64a8ec85dd0da8814f25dfbbf949ba436395397
Omolemo Mashigo <omomashigo@gmail.com>
2025-05-21 20:28:09 +0200

    Filtering @Query using Predicate

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \User.name) var users: [User] //a query sorting by our users' names
+    @Query(filter: #Predicate<User> { user in
+        if user.name.localizedStandardContains("R"){
+            if user.city == "Pretoria"{
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }, sort: \User.name) var users: [User] //a query sorting by our users' names
+    
     @State private var path = [User]()
     var body: some View {
         NavigationStack(path: $path){
@@ -26,9 +37,18 @@ struct ContentView: View {
             
             .toolbar{
                 Button("add user", systemImage: "plus"){
-                    let user = User(name: "", city: "", date: .now)
-                    modelContext.insert(user)
-                    path = [user]
+                    try? modelContext.delete(model: User.self)// delete all existing user objects
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", date: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Parks", city: "NYC", date: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Omolemo Halman Martin Mashigo", city: "Pretoria", date: .now.addingTimeInterval(86400 * -10))
+                    let fourth = User(name: "Skepta", city: "London", date: .now.addingTimeInterval(86400 * -10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                    
                 }
             }
         }
